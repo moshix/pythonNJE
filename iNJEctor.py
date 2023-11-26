a102e5421017ea0f542748a92f0d1ae1aa8a886c
moshix <moshix@gmail.com>
2023-11-26 04:26:43 -0600

    Update iNJEctor.py

--- iNJEctor.py
+++ iNJEctor.py
@@ -1,120 +1,63 @@
 #!/usr/bin/python
+#
+# Executes the JCL provided in the first argument
+# as the user provided as the second argument
+# and displays the information/contents to stdout
 
-## NJE NMR Command Sender
-## Allows for sending commands to z/OS NJE
-## requires OHOST and RHOST
-
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#########
-#
-# This program uses the NJE python library to sent command messages between NJE nodes
-# example:
-# $ ./iNJEctor.py 10.10.0.200 WASHDC NEWYORK "\$D NODE" --pass 'security' -d
-# output:
-# 12.21.15          $HASP826 NODE(1)
-# 12.21.15          $HASP826 NODE(1)  NAME=NEWYORK,STATUS=(OWNNODE),TRANSMIT=BOTH,
-# 12.21.15          $HASP826          RECEIVE=BOTH,HOLD=NONE
-# 12.21.15          $HASP826 NODE(2)
-# 12.21.15          $HASP826 NODE(2)  NAME=WASHDC,STATUS=(VIA/LNE1),TRANSMIT=BOTH,
-# 12.21.15          $HASP826          RECEIVE=BOTH,HOLD=NONE
-#
+# By Philip Young (c) 2016
+# MIT License
 
 import njelib
-import argparse
 import sys
-import signal
 
-class c:
-    BLUE = '\033[94m'
-    DARKBLUE = '\033[0;34m'
-    PURPLE = '\033[95m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    WHITE = '\033[1;37m'
-    ENDC = '\033[0m'
-    DARKGREY = '\033[1;30m'
-
-
-    def disable(self):
-        self.BLUE = ''
-        self.GREEN = ''
-        self.YELLOW = ''
-        self.DARKBLUE = ''
-        self.PURPLE = ''
-        self.WHITE= ''
-        self.RED = ''
-        self.ENDC = ''
-
-def signal_handler(signal, frame):
-        print (c.ENDC+ "\n( PACMAN Death Sound )\n")
-        sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
-#start argument parser
-parser = argparse.ArgumentParser(description='iNJEctor takes a target host, target NJE hostname and your own NJE hostname and send JES2 commands to the target. Displays the output to stdout.\n See: http://www-01.ibm.com/support/knowledgecenter/SSLTBW_2.1.0/com.ibm.zos.v2r1.hasa200/has2cmdr.htm for a list of commands.')
-parser.add_argument('target',help='The z/OS Mainframe NJE Server IP or Hostname')
-parser.add_argument('ohost',help='Name of the host you\'re sending the control record as. Note that both ohost and rhost must be valid.')
-parser.add_argument('rhost',help='Name of the host you expect to send the command to. Note that both ohost and rhost must be valid.')
-parser.add_argument('command',help='JES2 or console command or message you wish to send. ')
-parser.add_argument('-p','--port',help='The NJE server port. Default is 175', dest='port', default=175, type=int)
-parser.add_argument('-m','--message',help='Send as message instead of command.',dest='msg',default=False,action='store_true')
-parser.add_argument('--pass', help='Use this flag to provide a password for sigon', dest='password', default='')
-parser.add_argument('-u','--user', help='User to send message to (instead of default console)', dest='user', default='')
-parser.add_argument('-d','--debug',help='Show debug information. Displays A LOT of information',default=False,dest='debug',action='store_true')
-parser.add_argument('-q','--quiet',help='Do not display the logo',default=False,dest='quiet',action='store_true')
-args = parser.parse_args()
-
-if not args.quiet:
-    print (c.GREEN+'''        _ '''+c.RED+'''  _   __      __  ______'''+c.GREEN+'''      __
-       (_)'''+c.RED+''' / | / /     / / / ____/'''+c.GREEN+''' ____/ / ____  _____
-      / / '''+c.RED+'''/  |/ / _   / / / __/  '''+c.GREEN+'''/ ___/ __/ __ \/ ___/
-     / / '''+c.RED+'''/ /|  / / /_/ / / /___ '''+c.GREEN+'''/ /__/ /_/ /_/ / /
-    /_/ '''+c.RED+'''/_/ |_/  \____/ /_____/ '''+c.GREEN+'''\___/\__/\____/_/
-         The JES2 NJE Command Injector\n ''' + c.ENDC)
-
-
-nje = njelib.NJE(args.ohost,args.rhost)
-
-if not args.quiet: print ('[+] Signing on to', args.target,":", args.port)
-
-if args.debug:
-    nje.set_debuglevel(1)
-
-t = nje.session(host=args.target,port=args.port, timeout=2, password=args.password)
-
-if t:
-    if not args.quiet: print ('[+] Signon to', nje.host ,'Complete')
-else:
-    print ('[!] Signon to', nje.host ,'Failed!\n    Enable debugging to see why.')
+if len(sys.argv) < 6:
+    print ('Usage: ./jcl.py OHOST RHOST ip filname username [password]')
     sys.exit(-1)
-
-if not args.msg:
-    if not args.debug and not args.quiet: print ("[+] Sending Command:", args.command)
-    nmr = nje.sendCommand(args.command)
-    if not args.quiet: print ("[+] Reply Received:")
-    print ("\n", nmr)
+    
+print ("[+] OHOST:", sys.argv[1])
+print ("[+] RHOST:", sys.argv[2])
+print ("[+] IP   :", sys.argv[3])
+print ("[+] File :", sys.argv[4])
+print ("[+] User :", sys.argv[5])
+if len(sys.argv) > 6:
+    print ("[+] Pass :", sys.argv[6])
+    pwd = sys.argv[6]
 else:
-    if not args.debug and not args.quiet:
-        if args.user:
-            print ("[+] Sending Message (",args.command,") to user: ", args.user)
-        else:
-            print ("[+] Sending Message:", args.command)
-    if len(args.user) > 0:
-        nje.sendMessage(args.command, args.user)
-    else:
-        nje.sendMessage(args.command)
-    if not args.debug: print ("[+] Message sent")
+    pwd = ''
+
+
+nje = njelib.NJE(sys.argv[1],sys.argv[2])
+#nje.set_debuglevel(1)
+t = nje.session(host=sys.argv[3],port=175, timeout=2, password=pwd)
+# Notice the use of the password here to connect.
+if not t:
+    print ("[!] Could not connect")
+    sys.exit(1)
+
+print ("[+] Connected")
+print ("===================")
+print ("[+] Sending file:", sys.argv[4])
+with open (sys.argv[4], "r") as myfile:
+            data=myfile.readlines()
+print ("---------10--------20--------30---------40---------50---------60---------70---------80\n")
+for l in data:
+    print (l.strip("\n"))
+print ("\n---------10--------20--------30---------40---------50---------60---------70---------80")
+nje.sendJCL(sys.argv[4], sys.argv[5])
+print ("===================")
+print ("[+] Response Received")
+if len(nje.getNMR()) > 0:
+    print ("[+] NMR Records")
+
+print ("===================")
+for record in nje.getNMR():
+    if 'NMRUSER' in record:
+        print ("[+] User Message")
+        print ("[+] To User:", record['NMRUSER'])
+        print ("[+] Message:", record['NMRMSG'])
+
+print ("===================")
+print ("[+] Records in SYSOUT:")
+for record in nje.getSYSOUT():
+    if 'Record' in record:
+        print (record['Record'])
